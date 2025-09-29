2e30fbb6c88677a0e42995f6df48d90aa1c98ec5
Sergey-Gerasimo <gerasimovsergey2004@gmail.com>
2025-09-29 22:43:12 +0300

    fix: Исправил багу со значениями по умолчанию
    Исправил багу со значениями по умолчанию в классе User

--- app/domain/entities/base/user.py
+++ app/domain/entities/base/user.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from enum import Enum
+from typing import Optional
 import uuid
 
 from domain.values.Username import UserName
@@ -8,11 +9,11 @@ from domain.values.hashed_password import HashedPasswordSHA256
 
 @dataclass()
 class User:
-    id: str
     username: UserName
     password_hash: HashedPasswordSHA256
     role: "UserRole" = None
     is_active: bool = True
+    id: Optional[str] = None
 
 
 class UserRole(str, Enum):
