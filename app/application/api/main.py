31c907d06c04a34feabbe2d5d297bdbc8bae9fb6
Sergey-Gerasimo <gerasimovsergey2004@gmail.com>
2025-09-29 13:57:25 +0300

    dev: Добавил жизненный цикл приложения
    Добавил жизненный цикл приложения. В нем реализовал создание и удалени таблиц после завершения работы программы(в проде потом поправлю)

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -1,4 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
+
+from infra.database import async_engine, create_tables, drop_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Жизненный цикл приложения.
+
+    """
+    await create_tables(async_engine)
+    yield
+    await drop_tables(async_engine)
 
 
 def create_app():
@@ -6,4 +21,5 @@ def create_app():
         title="Industrial-Digital-Polygon",
         docs_url="/api/docs",
         description="Цифровой полигон: где станки общаются JSON'ом, а сменный мастер требует 'сделать красиво'",
+        lifespan=lifespan,
     )
